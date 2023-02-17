1f859d83893de9828de870faf333cd6d34cdc023
Yuhao <hello@chenyuhao.me>
2023-02-16 21:55:06 -0800

    Initial Commit

---
+++ ArcadeKit/ArcadeKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcadeKitApp.swift
+//  ArcadeKit
+//
+//  Created by Yuhao Chen on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ArcadeKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArcadeKit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ArcadeKit
+//
+//  Created by Yuhao Chen on 2/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
